d29d1d5c19e7b604646a11ffeb2f39bbaaa8f09a
Harshit112022 <128985969+Harshit112022@users.noreply.github.com>
2024-04-29 11:31:46 +0000

    Data Type

--- 01Basic/test.js
+++ 01Basic/test.js
@@ -1,1 +1,15 @@
-console.log("Harshit");
+var str ="Harshit"
+console.log(str);
+// ---------------------------------------------------- Variable -------------------------------------------------------------------------------------
+let name; // value is undefine 
+console.log(name);
+var interger = 125;
+console.log(interger);
+
+//-------------------------------------------------type Of Variable-------------------------------------------------------------------------------------
+console.log(typeof str );//string type
+console.log(typeof name);//undefine
+console.log(typeof interger );// number type
+console.log(typeof null);//obj
+console.log(typeof undefined);//undefined type
+
